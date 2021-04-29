4e1baeefb12ee5fc355d1a91d72209a159f8870f
Oscark021 <oscarvelasco169@gmail.com>
2021-04-29 17:32:20 -0500

    :sparkles: Llamada a la API
    Se realizo la llamada de la API exitosamente :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
 
 function App() {
+  const [dog, setDog] = useState({})
+  const [upload, setUpload]= useState({})
+  useEffect(() =>{
+    fetch('https://dog.ceo/api/breeds/image/random', {method: 'GET'})
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setDog(data)
+    })
+    
+  },[upload])
+ 
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 ClassName="api_h2">PRACTICA "CONSUMO DE API´S"</h3>
+      <Paper elevation={6}>
+      <img src={dog.message} alt=""/> 
+      </Paper>
+      <Button variant="contained" color="secondary" onClick={() => setUpload(upload + 1)} > 
+        Actualizar!
+      </Button>
     </div>
   );
 }
